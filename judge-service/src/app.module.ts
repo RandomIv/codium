82dcb146f4a0b1aaca9528c7fcaf673f3240f405
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2025-12-05 18:29:50 +0200

    feat(judge-service): integrate ConfigModule and create a connection to api

--- judge-service/src/app.module.ts
+++ judge-service/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JudgeProcessor } from './judge.processor';
 import { BullModule } from '@nestjs/bullmq';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     BullModule.forRoot({ connection: { host: 'localhost', port: 6379 } }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     BullModule.registerQueue({ name: 'judge-queue' }),
   ],
   controllers: [],
--- judge-service/src/judge.processor.ts
+++ judge-service/src/judge.processor.ts
@@ -1,11 +1,44 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
+import { ConfigService } from '@nestjs/config';
 
 @Processor('judge-queue')
 export class JudgeProcessor extends WorkerHost {
-  async process(job: Job<any, any, string>) {
-    console.log(`Judge received job#${job.id}`);
-    console.log('Code: ', job.data.code);
-    return {};
+  constructor(private readonly configService: ConfigService) {
+    super();
+  }
+  async process(job: Job) {
+    const { problemId, code, language } = job.data;
+    const submissionId = job.id;
+
+    const apiUrl = this.configService.getOrThrow('API_URL').replace(/\/$/, '');
+    const systemApiKey = this.configService.getOrThrow('SYSTEM_API_KEY');
+    const url = `${apiUrl}/problems/system/${problemId}`;
+
+    try {
+      const response = await fetch(url, {
+        headers: {
+          'x-system-api-key': systemApiKey,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch problem. Status: ${response.status} ${response.statusText}`,
+        );
+      }
+
+      const problemData = await response.json();
+      console.log(
+        ` Loaded problem ${problemData.title}. Tests: ${problemData.testCases.length}`,
+      );
+
+      //check code here
+
+      return { status: 'Processing link established' };
+    } catch (error) {
+      console.error(`Judge failed for #${submissionId}:`, error.message);
+      throw error;
+    }
   }
 }
