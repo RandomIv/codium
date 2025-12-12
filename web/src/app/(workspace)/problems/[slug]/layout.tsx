f66f437d8d46fe9264b50953bf06810bcd55cbf5
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2025-12-12 18:56:11 +0200

    refactor(web): remove StatusBar component from ProblemWorkspace and clean up layout

--- web/src/app/(workspace)/problems/[slug]/layout.tsx
+++ web/src/app/(workspace)/problems/[slug]/layout.tsx
@@ -1,5 +1,4 @@
 import ProblemHeader from './_components/ProblemHeader';
-import StatusBar from '@/app/(workspace)/problems/[slug]/_components/StatusBar';
 
 export default function WorkspaceLayout({
   children,
@@ -10,7 +9,6 @@ export default function WorkspaceLayout({
     <div className="flex h-screen w-screen flex-col overflow-hidden bg-background">
       <ProblemHeader />
       <main className="flex-1 overflow-hidden">{children}</main>
-      <StatusBar></StatusBar>
     </div>
   );
 }
